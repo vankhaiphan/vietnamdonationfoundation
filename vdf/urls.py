08763bc49d57dab0a8fb09c0105a44052f1bbd6e
Roa <minhchaumvp@gmail.com>
2020-05-29 11:36:53 +0700

    adjust link

--- vdf/urls.py
+++ vdf/urls.py
@@ -4,13 +4,13 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("about", views.about, name="about"),
-    path("articles", views.about, name="articles"),
-    path("explore_pages", views.about, name="explore_pages"),
-    path("explore_result", views.about, name="explore_result"),
-    path("explore", views.about, name="explore"),
-    path("login", views.about, name="login"),
-    path("project_detail", views.about, name="project_detail"),
-    path("register", views.about, name="register"),
-    path("taochiendich", views.about, name="taochiendich"),
+    path("articles", views.article, name="articles"),
+    path("explore_pages", views.explore_pages, name="explore_pages"),
+    path("explore_result", views.explore_result, name="explore_result"),
+    path("explore", views.explore, name="explore"),
+    path("login", views.login, name="login"),
+    path("project_detail", views.project_detail, name="project_detail"),
+    path("register", views.register, name="register"),
+    path("taochiendich", views.taochiendich, name="taochiendich"),
     # path("<str:name>", views.greet, name="greet")
 ]
