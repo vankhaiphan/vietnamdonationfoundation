e67f425f7c32674b125983b42cfa4cde21fecd5c
Mihawkkkk <pvkhai98dn@gmail.com>
2020-05-29 11:11:04 +0700

    Add route for html pages

--- vdf/urls.py
+++ vdf/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+    path("articles", views.about, name="articles"),
+    path("explore_pages", views.about, name="explore_pages"),
+    path("explore_result", views.about, name="explore_result"),
+    path("explore", views.about, name="explore"),
+    path("login", views.about, name="login"),
+    path("project_detail", views.about, name="project_detail"),
+    path("register", views.about, name="register"),
+    path("taochiendich", views.about, name="taochiendich"),
     # path("<str:name>", views.greet, name="greet")
 ]
--- vdf/views.py
+++ vdf/views.py
@@ -5,7 +5,35 @@ from django.shortcuts import render
 def index(request):
     return render(request, "vdf/index.html")
 
-def greet(request, name):
-    return render(request, "vdf/greet.html", {
-        "name":name.capitalize()
-    })
+def about(request):
+    return render(request, "vdf/about.html")
+
+def articles(request):
+    return render(request, "vdf/articles.html")
+
+def explore_pages(request):
+    return render(request, "vdf/explore_pages.html")
+
+def explore_result(request):
+    return render(request, "vdf/explore_result.html")
+
+def explore(request):
+    return render(request, "vdf/explore.html")
+
+def login(request):
+    return render(request, "vdf/login.html")
+
+def project_detail(request):
+    return render(request, "vdf/project_detail.html")
+
+def register(request):
+    return render(request, "vdf/register.html")
+
+def taochiendich(request):
+    return render(request, "vdf/taochiendich.html")
+
+
+# def greet(request, name):
+#     return render(request, "vdf/greet.html", {
+#         "name":name.capitalize()
+#     })
